9b6dcf89e5ebaa495cad91b935c35fc2c961813b
Falderian <nikitastaselovich@gmail.com>
2022-05-10 21:45:01 +0300

    fix: Shift is working!

--- components/scripts/inputForVirtKey.js
+++ components/scripts/inputForVirtKey.js
@@ -4,9 +4,10 @@ import { currentLang } from "./changeLang";
 
 
 let shiftIsPressed;
+
 const addListenerForClick = () => {
     const allTD = document.querySelectorAll('td');
-    
+    const allPar = document.querySelectorAll('p');
     allTD.forEach(item => item.addEventListener('click', function(event) {
         if(event.target.innerText.length <= 1) textarea.textContent += event.target.innerText;
         switch (event.target.innerText) {
@@ -14,16 +15,25 @@ const addListenerForClick = () => {
                 let str = textarea.textContent;
                 textarea.innerHTML = str.slice(0, -1);
         }
-
-        if(event.target.innerHTML === 'Shift') {
-            document.getElementById(42).classList.add('active-key');
-            const allPar = document.querySelectorAll('p');
-            console.log(localStorage.getItem(currentLang));
-            for(let i = 0; i < 64; i++) {
-                allPar[i].innerHTML = dataKeys.keyboard[i].shiftEn;
-            }
-        }
+        isShiftPressed(event);
     }));    
+}
+
+const isShiftPressed = (event) => {
+    const allPar = document.querySelectorAll('p');
+    if(event.target.innerHTML === 'Shift') {
+        document.getElementById(42).classList.add('active-key');        
+        console.log(localStorage.getItem(currentLang));
+        for(let i = 0; i < 64; i++) {
+            allPar[i].innerHTML = dataKeys.keyboard[i].shiftEn;
+        }
+    }
+    else {
+        document.getElementById(42).classList.remove('active-key');
+        for(let i = 0; i < 64; i++) {
+            allPar[i].innerHTML = dataKeys.keyboard[i].en;
+        }
+    }
 }
 
 
