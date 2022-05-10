4764317ad37af7d32df2e78443de2c87517925eb
Falderian <nikitastaselovich@gmail.com>
2022-05-10 21:49:49 +0300

    fix: Shift iw working for RU

--- components/scripts/inputForVirtKey.js
+++ components/scripts/inputForVirtKey.js
@@ -4,6 +4,7 @@ import { currentLang } from "./changeLang";
 
 
 let shiftIsPressed;
+let currLang = localStorage.getItem('currentLang');
 
 const addListenerForClick = () => {
     const allTD = document.querySelectorAll('td');
@@ -15,23 +16,44 @@ const addListenerForClick = () => {
                 let str = textarea.textContent;
                 textarea.innerHTML = str.slice(0, -1);
         }
-        isShiftPressed(event);
+        currLang === 'en' ? isShiftPressedEn(event) : isShiftPressedRu(event);
     }));    
 }
 
-const isShiftPressed = (event) => {
+const isShiftPressedEn = (event) => {
     const allPar = document.querySelectorAll('p');
     if(event.target.innerHTML === 'Shift') {
-        document.getElementById(42).classList.add('active-key');        
-        console.log(localStorage.getItem(currentLang));
+        document.getElementById(42).classList.add('active-key');  
+        document.getElementById(54).classList.add('active-key');     
+        console.log(currLang);
         for(let i = 0; i < 64; i++) {
             allPar[i].innerHTML = dataKeys.keyboard[i].shiftEn;
         }
     }
     else {
         document.getElementById(42).classList.remove('active-key');
+        document.getElementById(54).classList.remove('active-key');
         for(let i = 0; i < 64; i++) {
             allPar[i].innerHTML = dataKeys.keyboard[i].en;
+        }
+    }
+}
+
+const isShiftPressedRu = (event) => {
+    const allPar = document.querySelectorAll('p');
+    if(event.target.innerHTML === 'Shift') {
+        document.getElementById(42).classList.add('active-key');  
+        document.getElementById(54).classList.add('active-key');     
+        console.log(currLang);
+        for(let i = 0; i < 64; i++) {
+            allPar[i].innerHTML = dataKeys.keyboard[i].shiftRu;
+        }
+    }
+    else {
+        document.getElementById(42).classList.remove('active-key');
+        document.getElementById(54).classList.remove('active-key');
+        for(let i = 0; i < 64; i++) {
+            allPar[i].innerHTML = dataKeys.keyboard[i].ru;
         }
     }
 }
